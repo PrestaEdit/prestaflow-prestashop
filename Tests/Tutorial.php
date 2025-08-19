c8fc531226c63cfc801e8b2e4e082409d50ebcb4
Jonathan Danse <j.danse@prestaedit.com>
2025-08-19 14:18:32 +0200

    Rename and update First test to Tutorial
    Renamed First.php to Tutorial.php, updated the class name and description, added draft and groups properties, and included the DoNothing scenario in the test suite.

--- Tests/Tutorial.php
+++ Tests/Tutorial.php
@@ -3,17 +3,22 @@
 namespace Tests;
 
 use PrestaFlow\Library\Tests\TestsSuite;
+use Shop\Tests\Scenarios\DoNothing;
 
-class First extends TestsSuite
+class Tutorial extends TestsSuite
 {
+    public $draft = true;
+    public $groups = 'tutorial';
+
     public function init()
     {
         $this
-        ->describe('PrestaFlow: my first scenario')
+        ->describe('PrestaFlow: Tutorial')
         ->todo('will do nothing (again)', function () {
         })
         ->skip('will do nothing (again)', function () {
         })
+        ->scenario(DoNothing::class)
         ;
     }
 }
